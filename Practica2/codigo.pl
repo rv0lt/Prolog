a5f36ff7a6ca98b974769dca558a17a5202e41d8
athanatos96 <51208773+athanatos96@users.noreply.github.com>
2019-06-07 20:42:13 +0200

    Add files via upload
    Codigo finalizado con los comentarios

--- Practica2/codigo.pl
+++ Practica2/codigo.pl
@@ -2,16 +2,20 @@ alumno_prode('Parra','Garcia','Alejandro Carmelo','Y16I028').
 alumno_prode('Vladimirov','Stoyanov','Petko','Y16I019').
 alumno_prode('Revuelta','Martinez','Alvaro','Y16I009').
 
-%menor(A,B,Comp,M):-
-%	functor(Comp,X,0),
-%        ( compare(X, A, B) -> M=A ; M=B ).
 
+%------------------------------------------------------------------------------------------------
+%Parte 1
+%Menor crea una estructura X de aridad 2 con el argumento Comp  como functor, e introduce
+%A y B como argumentos de la estructura para hacer un call y así poder evaluar la expresión
 menor(A,B,Comp,M):-
 	functor(X,Comp,2),
 	arg(1,X,A),
 	arg(2,X,B),
 	(call(X) -> M=A; M=B ).
 
+
+%------------------------------------------------------------------------------------------------
+%Parte 2
 
 %Caso base en el que si A o B son variables libres es true,
 %pues "Una variable libre es igual a cualquier otro termino"
@@ -58,14 +62,14 @@ menor_o_igual_aux(A,_,N):-
 	functor(A,_,La),
 	N1 is N-1,
 	La =:= N1.
-%Sacamos el termino N de A y de B, si nos iguales hago recursividad avanzando N para comparar el siguiente termmino.
+%Sacamos el termino N de A y de B, si son iguales hago recursividad avanzando N para comparar el siguiente termino.
 menor_o_igual_aux(A,B,N):-
 	arg(N,A,ElemA),
 	arg(N,B,ElemB),
 	soy_igual(ElemA,ElemB),!,
 	N1 is N+1,
 	menor_o_igual_aux(A,B,N1).
-%En este caso sabemos que el termino N de A y B no es igual, por lo que si es menor devolvemos true yno hace falta comparar mas.
+%En este caso sabemos que el termino N de A y B no es igual, por lo que si es menor devolvemos true y no hace falta comparar mas.
 menor_o_igual_aux(A,B,N):-
 	arg(N,A,ElemA),
 	arg(N,B,ElemB),
@@ -75,7 +79,7 @@ menor_o_igual_aux(A,B,N):-
 
 %Predicado auxiliar que se usa para evaluar si dos terminos son iguales
 %Dos terminos son iguales si alguno de ellos es variable libre.
-%Dos terminos son iguales los nombres de ambos, tienen la misma ariedad y sus argumentos son identicos.
+%Dos terminos son iguales si los nombres de ambos son identicos, tienen la misma ariedad y sus argumentos son identicos.
 soy_igual(A,B):-
 	var(A);
 	var(B).
@@ -87,6 +91,7 @@ soy_igual(A,B):-
 	La =:= Lb,
 	soy_igual_aux(A,B,1).
 %caso base en el que ya se han comparado todos los argumentos, por lo tanto son iguales
+%La ariedad de A es 0
 soy_igual_aux(A,_,N):-
 	functor(A,_,La),
 	N1 is N-1,
@@ -103,11 +108,20 @@ soy_igual_aux(A,B,N):-
 
 
 
+%-------------------------------------------------------------------------------------------------------------------------
+%Parte 3
+
+%%%%%%%%%%%%%%%%%%%
+%%%lista_hojas/2%%%
+%%%%%%%%%%%%%%%%%%%
 
 lista_hojas([],[]).
 lista_hojas([H|L],[tree(H,void,void)|HOJAS]):-
-  lista_hojas(L,HOJAS).
+	lista_hojas(L,HOJAS).
 
+%%%%%%%%%%%%%%%%%%%
+%%%hojas_arbol/3%%%
+%%%%%%%%%%%%%%%%%%%
 
 %Si esta vacio la lisa de hojas no hay arbol.
 hojas_arbol([],_,_).
@@ -117,27 +131,32 @@ hojas_arbol([X],_,X).
 hojas_arbol(Lista,Comp,Arbol):-
 	hojas_arbol_aux(Lista,[],Comp,Arbol).
 
-%El primer termino es una list con las hojas que quedan por fusionarse, una vez se fususionan se pasan a la segunda lista, cuando la primera lista esta vacia se pasan todos los elementos de la segunda a la primera y se vacia la segunda. Si queda 1 elemento en la primera lista este se pone al final de la segunda y despues se pasa la segunda lista a la primera y se vacia la segunda.
-%El proceso se repite hasta que solo quede un elemento en la primera, que significa que ya esta el arbol creado
+%El primer termino es una lista con las hojas que quedan por fusionarse, una vez se fususionan se pasan a la
+%segunda lista, cuando la primera lista esta vacia se pasan todos los elementos de la segunda a la primera y
+%se vacia la segunda. Si queda 1 elemento en la primera lista este se pone al final de la segunda y despues se
+%pasa la segunda lista a la primera y se vacia la segunda.
+%El proceso se repite hasta que solo quede un elemento en la primera y la segunda lista este vacia, que significa que ya esta el arbol creado
 
 
-%Si solo queda 1 hoja se devuelve esa como el arbol
+%Si solo queda 1 hoja, y la segunda esta vacia, se devuelve esa hoja como el arbol
 hojas_arbol_aux([X],[],_,X).
 %Si la primera lista esta vacia se vuelca el contenido de la segunda en la primera y se vacia la segunda. Y se vuelve a llamar a hojas_arbol_aux/4
 hojas_arbol_aux([],Lista,Comp,Arbol):-
 	hojas_arbol_aux(Lista,[],Comp,Arbol).
-%Si solo queda 1 elemento en la primera lista se añade al final de la segunda. Posteriormente la segunda lista se vuelca en la primera y se vacia la segunda. Y se vuelve a llamar a hojas_arbol_aux/4
+%Si solo queda 1 elemento en la primera lista se añade al final de la segunda. Posteriormente la segunda lista se vuelca en la primera y se vacia la segunda.
+%Y se vuelve a llamar a hojas_arbol_aux/4
 hojas_arbol_aux([X],Lista,Comp,Arbol):-
 	insertar(Lista,X,Lista1),
 	hojas_arbol_aux(Lista1,[],Comp,Arbol).
-%si hay mas elementos se sacan los dos priemos y se construye un arbol con ellos. Despues se añaden al final de la  segunda lista. Se vuelve a llamr a hojas_arbol_aux/4, pero en la primera lista ya no estan los dos primeros elementos.
+%si hay mas elementos se sacan los dos priemos y se construye un arbol con ellos. Despues se añaden al final de la  segunda lista.
+%Se vuelve a llamar a hojas_arbol_aux/4, pero en la primera lista ya no estan los dos primeros elementos.
 hojas_arbol_aux([tree(E_A,H1_A,H2_A),tree(E_B,H1_B,H2_B)|Hojas],Lista,Comp,Arbol):-
 	menor(E_A,E_B,Comp,M),
 	insertar(Lista,tree(M,tree(E_A,H1_A,H2_A),tree(E_B,H1_B,H2_B)),Lista1),
 	hojas_arbol_aux(Hojas,Lista1,Comp,Arbol).
 	
 
-%nserta un Item al final de una lista y lo devuelve en SOlucion.
+%Inserta un Item al final de una lista y lo devuelve en Solucion.
 insertar(Lista,Item,Solucion):-
 	length(Lista,X),
 	X =:= 0,
@@ -146,20 +165,35 @@ insertar([E|Lista],Item,[E|Solucion]):-
 	insertar(Lista,Item,Solucion).
 
 
+%%%%%%%%%%%%%%%%%%
+%%%ordenacion/3%%%
+%%%%%%%%%%%%%%%%%%
+
+%Predicado que genera una lista ordenada a partir de un arbol
+%Si el arbol esta vacio se devuelve una lista vacia
 ordenacion(void,_,[]).
+%Llamamos a reflotar/3 que dado un arbol y un Comp genera un arbol reflotado.
+%despues hacemos recursividad, con el nuevo arbol. Vamos añadiendo los elemntos a la lista a la que volvemos
 ordenacion(tree(N,I,D), Comp, [N|Orden]):-
 	reflotar(tree(N,I,D),Comp,ArbolReflotado),
 	ordenacion(ArbolReflotado, Comp, Orden).
 
 
-
+%predicado auxiliar que usamos para relotar un arbol
+%Caso base en el que solo hay una hoja en el arbol, el arbol resultado es void.
 reflotar(tree(_,void,void),_,X):-
 	X=void.
+%Casos en los que uno de los hijos es la hoja con el elemnto.
+%En este caso se devuelve el otro hijo, que puede ser una sola hoja o un arbol.
 reflotar(tree(E,tree(E,void,void),tree(E2,I2,D2)),_,X):-
 	X = tree(E2,I2,D2).
 reflotar(tree(E,tree(E1,I1,D1),tree(E,void,void)),_,X):-
 	X = tree(E1,I1,D1).
 
+%Se distingue dos casos, en el cual el nodo con la hoja esta en un lado o en otro.
+%Una vez sabemos por que hijo se encuentra el nodo hoja llamams a reflotar con ese hijo (que puede ser una sola hoja o un arbol)
+%Creamos un nuevo arbol con el otro hijo y el arbol que nos ha devuelto la llamada a reflorar y ponemos de elemento de la raiz de este
+%nuevo arbol el elemento que nos devuelve menor entre el otro hijo y el del nuevo arbol.
 reflotar(tree(E,tree(E1,I1,D1),tree(E2,I2,D2)),Comp,X):-
 	(E == E1 ->
 	 (reflotar(tree(E1,I1,D1),Comp,tree(X1Elem,XI1,XD1)),
@@ -172,12 +206,14 @@ reflotar(tree(E,tree(E1,I1,D1),tree(E2,I2,D2)),Comp,X):-
 	  )
 	).
 
+%%%%%%%%%%%%%%%%%%%
+%%%%%ordenar/3%%%%%
+%%%%%%%%%%%%%%%%%%%
+%Dada una Lista y un Comp nos devuelve una lista ordenada en Orden
+%Hace la llamada a lista_hojas/2 que nos crea unas hojas en funcion de los elementos de la lista
+%llama a hojas_arbol, con la lista de hojas previamente creadas y el comparador, esta llamada nos devuelve un arbol flotante
+%Llamada a ordenacion con el Arbol y el Comp, este predicado nos genera una lista ordenada a partir del Arbol
 ordenar(Lista,Comp,Orden):-
 	lista_hojas(Lista,Hojas),
 	hojas_arbol(Hojas,Comp,Arbol),
 	ordenacion(Arbol,Comp,Orden).
-
-
-
-
-
